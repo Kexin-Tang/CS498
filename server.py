6e2e5dd0c66afb29dfd7008a9f54e62210e0c8a0
Kexin-Tang <KexinTang.0210@gmail.com>
2022-04-28 21:36:19 -0500

    upload

--- server.py
+++ server.py
@@ -11,7 +11,12 @@ from util import checkAvailable, getCityCode, countReview, noAvailableNeighborho
 
 app = Flask(__name__)
 
-pool = redis.ConnectionPool(host='localhost', port=6379, decode_responses=True)   # host是redis主机，需要redis服务端和客户端都起着 redis默认端口是6379
+pool = redis.ConnectionPool(
+	host='localhost',
+	port=6379,
+	password="12345",
+	decode_responses=True
+)   # host是redis主机，需要redis服务端和客户端都起着 redis默认端口是6379
 r = redis.Redis(connection_pool=pool)
 
 @app.route('/')
